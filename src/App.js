779a894a15a3376e2a2cc3e64c6369ff2b9bbb36
Antonela Borgogno <antonelaborgogno@gmail.com>
2023-10-19 23:58:01 -0300

    lista imagen, seleccion imagen, material UI

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import Visualizador from './components/Visualizador/Visualizador'
 
 function App() {
   return (
     <div className="App">
+      <Visualizador/>
      
     </div>
   );
